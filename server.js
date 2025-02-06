1a3de33b8e78005ec557be7bc7235456a1360367
prayagx098 <prayagjishnu123@gmail.com>
2025-02-06 23:52:32 +0530

    test

--- server.js
+++ server.js
@@ -13,3 +13,14 @@ app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+// Test Route
+app.get("/api/test", (req, res) => {
+    res.json({ message: "Connection Successful! 🚀", status: "ok" });
+  });
+  
+// Start Server
+app.listen(PORT, () => {
+console.log(`✅ Server running on port ${PORT}`);
+});
